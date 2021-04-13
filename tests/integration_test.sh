618d0ba4a7e5d424cdec1930c6749c312a6e3f8d
James King <jcking1034@g.ucla.edu>
2021-04-13 19:20:39 +0000

    Added integration test that:
    - Creates a temporary directory and relevant test files (ex. config file for the server)
    - Starts the server
    - Sends a request to the server
    - Verifies that the server responded and that the response matches expectations
    - Cleans up generated files and stops the server
    
    The integration test is integrated with ctest/make test
    
    Change-Id: I2eec3bcf1650658402c776ecddd1371df8470be8

---
+++ tests/integration_test.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Variables
+TMP_DIR=$(mktemp -d -t TEST-XXXXXXXXXX)
+CONFIG_FILE="$TMP_DIR/config.txt"
+EXPECTED_FILE="$TMP_DIR/expected.txt"
+OUTPUT_FILE="$TMP_DIR/output.txt"
+EXECUTABLE="../build/bin/webserver"
+LOCAL_HOST="127.0.0.1"
+PORT="8080"
+
+# Setup
+
+# - Config File
+cat > "$CONFIG_FILE" << EOF
+server { listen $PORT; }
+EOF
+
+# - Expected File
+cat > "$EXPECTED_FILE" << EOF
+GET / HTTP/1.1
+Host: $LOCAL_HOST:$PORT
+User-Agent: curl/7.68.0
+Accept: */*
+
+EOF
+sed -i -e 's/$/\r/' "$EXPECTED_FILE"  # newlines should be CRLF
+
+# - Server
+"$EXECUTABLE" "$CONFIG_FILE" &
+SERVER_PID=$!
+sleep 1  # A short delay to allow time for the server to start
+
+# Perform Test
+
+# Check that web server handled request
+#   -s: Silent
+#   -S: Show error (even when silent)
+#   -o: Output file
+curl -s -S -o "$OUTPUT_FILE" "$LOCAL_HOST":"$PORT"
+CURL_RESULT=$?
+
+# Check that the response is normal
+#   -N: Treat absent file as empty
+diff -N "$EXPECTED_FILE" "$OUTPUT_FILE"
+DIFF_RESULT=$?
+
+# Cleanup
+rm -rf "$TMP_DIR"
+kill $SERVER_PID
+
+# Get Test Result
+# A successful test means that the web server received and handled an HTTP
+# request, and provided a response to echo the request back to the client. An
+# unsuccessful test would mean either the client could not receive a response
+# from the server, or that the response was different than what was expected.
+[ $CURL_RESULT -eq 0 ] && [ $DIFF_RESULT -eq 0 ]
+TEST_RESULT=$?
+exit $TEST_RESULT  # Exits with code 0 (success) or 1 (failure)
