69dd26a5910554672934fa238b1e35d86467a4ea
Avery Ezra <einhorn@g.ucla.edu>
2021-05-12 18:18:17 -0700

    Updated proxyhandler to actually get other pages
    Change-Id: Ic8aaa0728a43936cd3c6e1ed690e01ba8f0fbf8b

--- include/proxy_request_handler.h
+++ include/proxy_request_handler.h
@@ -2,9 +2,12 @@
 #define PROXY_HTTP_REQUEST_HANDLER_HPP
 
 #include <string>
+#include <boost/asio.hpp>
 
 #include "config_parser.h"
 #include "request_handler.h"
+
+namespace http = boost::beast::http;
 
 class proxy_request_handler : public request_handler
 {
@@ -12,11 +15,20 @@ public:
   explicit proxy_request_handler(std::string handler_location, const NginxConfig & handler_config);
 
   /// Handle a request and produce a reply.
-  boost::beast::http::response<boost::beast::http::string_body> handle_request(const boost::beast::http::request<boost::beast::http::string_body>& request);
+  http::response<http::string_body> handle_request(const http::request<http::string_body>& request);
 
 private:
   /// The url of the website to send a request to
   std::string host_name_;
+
+    /// Formulate a proper HTTP request to the website
+    std::string form_URI(const http::request<http::string_body>& request);
+
+    /// Find the code in the response headers
+    static int get_code(std::string headers);
+
+    static bool get_new_request(
+        std::string headers, std::string& new_host, std::string& new_path);
 
   /// The port number to issue requests to
   int host_port_;
--- src/proxy_request_handler.cc
+++ src/proxy_request_handler.cc
@@ -8,26 +8,171 @@
 
 #include "proxy_request_handler.h"
 
-proxy_request_handler::proxy_request_handler(std::string handler_location, const NginxConfig & handler_config)
-  : request_handler(handler_location, handler_config)
+proxy_request_handler::proxy_request_handler(
+    std::string handler_location, const NginxConfig & handler_config)
+    : request_handler(handler_location, handler_config)
 {
-  NginxConfigStatement * host_config_statement = NginxConfigParser::find_statement("host", &handler_config);
-  host_name_ = NginxConfigParser::parse_string(host_config_statement->tokens_[1]);
-
-  NginxConfigStatement * port_config_statement = NginxConfigParser::find_statement("port", &handler_config);
-  host_port_ = std::stoi(NginxConfigParser::parse_string(port_config_statement->tokens_[1]));
+    NginxConfigStatement * host_config_statement =
+        NginxConfigParser::find_statement("host", &handler_config);
+    host_name_ =
+        NginxConfigParser::parse_string(host_config_statement->tokens_[1]);
+    
+    NginxConfigStatement * port_config_statement = NginxConfigParser::find_statement("port", &handler_config);
+    host_port_ = std::stoi(NginxConfigParser::parse_string(port_config_statement->tokens_[1]));
 }
 
-boost::beast::http::response<boost::beast::http::string_body> proxy_request_handler::handle_request(const boost::beast::http::request<boost::beast::http::string_body>& request)
+http::response<http::string_body> proxy_request_handler::handle_request(
+    const http::request<http::string_body>& request)
 {
-  boost::beast::http::response<boost::beast::http::string_body> response;
+    http::response<http::string_body> response;
+    
+    // TODO replace fake response with call from server's response
+    std::string req_URI = form_URI(request);
+    BOOST_LOG_TRIVIAL(info) << "Proxy URI: " << req_URI;
 
-  // TODO replace fake response with call from server's response
-  response.body() += std::string("Place Holder for Request");
-  response.set("Content-Type", "text/plain");
-  response.prepare_payload();
+    boost::asio::ip::tcp::iostream stream(host_name_, "http");
+    stream << "GET " << req_URI << " HTTP/1.1\r\n";
+    stream << "Host: " << host_name_ << "\r\n";
+    stream << "Accept: */*\r\n";
+    stream << "Connection: close\r\n\r\n";
+    stream.flush();
 
-  BOOST_LOG_TRIVIAL(info) << "Prepared Proxy Request Response";
+    std::ostringstream ss;
+    ss << stream.rdbuf();
+    std::string result = ss.str();
 
-  return response;
+    int headers_end = result.find("\r\n\r\n");
+    std::string headers;
+    
+    if (headers_end != std::string::npos) {
+        headers = result.substr(0, headers_end + 4);
+        result = result.substr(headers_end + 4);
+    }
+        
+    int code = get_code(headers);
+    BOOST_LOG_TRIVIAL(debug) << "Response is: " << code;
+    while (code >= 300 && code <= 399) {
+        std::string new_host;
+        std::string new_path;
+        bool success = get_new_request(headers, new_host, new_path);
+        if (!success) return response;
+        BOOST_LOG_TRIVIAL(debug) << "300 redirect | Host: " << new_host
+                                 << " | Path: " << new_path;
+        boost::asio::ip::tcp::iostream redir_stream(new_host, "http");
+        redir_stream << "GET " << new_path << " HTTP/1.1\r\n";
+        redir_stream << "Host: " << new_host << "\r\n";
+        redir_stream << "Accept: */*\r\n";
+        redir_stream << "Connection: close\r\n\r\n";
+        redir_stream.flush();
+
+        std::ostringstream rss;
+        rss << redir_stream.rdbuf();
+        std::string redir_result = rss.str();
+
+        headers_end = redir_result.find("\r\n\r\n");
+        if (headers_end != std::string::npos) {
+            headers = redir_result.substr(0, headers_end + 4);
+            result = redir_result.substr(headers_end + 4);
+        }
+        
+        code = get_code(headers);
+        BOOST_LOG_TRIVIAL(debug) << "Response is: " << code;
+    }
+    
+    response.body() += result;
+    response.set("Content-Type", "text/plain");
+    response.prepare_payload();
+
+    BOOST_LOG_TRIVIAL(info) << "Prepared Proxy Request Response";
+    
+    return response;
+}
+
+
+bool proxy_request_handler::get_new_request(
+    std::string headers, std::string& new_host, std::string& new_path) {
+    bool success = false;
+    std::string space = " ";
+    std::string CRLF = "\r\n";
+    
+    std::string line;
+    std::string location;
+    while(true){
+        if (headers.length() == 0) break;
+        std::string line = headers.substr(0, headers.find(CRLF));
+        std::string token = line.substr(0, headers.find(space));
+        BOOST_LOG_TRIVIAL(trace) << "Examining line in proxy handler received header: " << line;
+        if (token.find("location:") != std::string::npos ||
+            token.find("Location:") != std::string::npos) {
+            line.erase(0, std::string("location:").length());
+            while(line[0] == ' ') line.erase(0,1);
+            while(line.find(" ") != std::string::npos) line.erase(line.find(" "));
+            success = true;
+            location = line;
+            break;
+        }
+        else headers.erase(0, line.length() + 2);
+    }
+    if (location.find("://") != std::string::npos) location.erase(0, location.find("://") + 3);
+    if (location.find("/") == std::string::npos) {
+        new_host = location;
+        new_path = "/";
+    }
+    else {
+        new_host = location.substr(0, location.find("/"));
+        new_path = location.substr(location.find("/"));
+    }
+    return success;
+
+}
+std::string proxy_request_handler::form_URI(const http::request<http::string_body>& request) {
+    std::string URI_to_req;
+    bool success = url_decode(std::string(request.target()), URI_to_req);
+    if (!success) return "/";
+
+    if (handler_location_.find_last_of('/') == handler_location_.length() - 1)
+        handler_location_.pop_back();
+
+    std::size_t prefix_pos = URI_to_req.find(handler_location_);
+    if (prefix_pos == std::string::npos) return "/";
+    URI_to_req.erase(0, handler_location_.length());
+    if (URI_to_req.length() == 0) return "/";
+    
+    return URI_to_req;
+}
+
+int proxy_request_handler::get_code(std::string headers){
+    std::string space = " ";
+    std::string CRLF = "\r\n";
+    int code = 200;
+
+    std::string token;
+    int token_len;
+    bool found_HTTP = false;
+    for (int i = 0; i < 20; i++){
+        std::string token_space = headers.substr(0, headers.find(space));
+        std::string token_CRLF = headers.substr(0, headers.find(CRLF));
+        
+        if (token_space.length() < token_CRLF.length()) token_len = token_space.length();
+        else token_len = token_CRLF.length();
+        
+        token = headers.substr(0, token_len);
+        headers.erase(0, token.length() + 1);
+
+        if (token.find("HTTP") != std::string::npos) {
+            found_HTTP = true;
+            continue;
+        }
+        if (found_HTTP){
+            found_HTTP = false;
+            try {
+                code = std::stoi(token);
+                break;
+            } catch (const std::invalid_argument& ia) {
+                BOOST_LOG_TRIVIAL(warning) << "Could not convert header token to status code";
+            }
+        }
+        
+    }
+    return code;
 }
